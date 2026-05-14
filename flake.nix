0ee82cd22c66ee6fd78996edf77f6cf852f6461b
haruki7049 <tontonkirikiri@gmail.com>
2026-05-14 10:55:17 +0900

    Update

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, lib, ... }:
+        let
+          nativeBuildInputs = [
+            pkgs.deno # Deno
+            pkgs.nil # Nix LSP
+          ];
+        in
+        {
+          treefmt = {
+            projectRootFile = ".git/config";
+
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # Deno
+            programs.deno.enable = true;
+
+            # GitHub Actions
+            programs.actionlint.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            inherit nativeBuildInputs;
+          };
+        };
+    };
+}
