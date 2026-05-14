5869cde525a96a9ad3fe3c17fb61ba61b2b0094b
haruki7049 <tontonkirikiri@gmail.com>
2026-05-14 11:45:54 +0900

    Update

--- monitan/components/Main.tsx
+++ monitan/components/Main.tsx
@@ -5,8 +5,12 @@ export function Main() {
       <p>This is a WIP web application</p>
 
       <address>
-        <a href="https://haruki7049.dev">haruki7049.dev</a>
-        <a href="https://github.com/haruki7049">github.com/haruki</a>
+        <p>
+          <a href="https://haruki7049.dev">haruki7049.dev</a>
+        </p>
+        <p>
+          <a href="https://github.com/haruki7049">github.com/haruki</a>
+        </p>
         <p>
           Git Repository:{" "}
           <a href="https://github.com/haruki7049/monitan">
