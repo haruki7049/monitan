4aeef37b39f36d1a5a07b8aead56234d41d7cab4
haruki7049 <tontonkirikiri@gmail.com>
2026-05-14 18:02:50 +0900

    Update

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 import { Hono } from "@hono/hono";
-import { Index } from "./monitan/index.tsx";
+import { Index } from "./monitan/components/index.tsx";
 const app = new Hono();
 
 app.get("/", (c) => c.html(<Index />));
--- monitan/components/index.tsx
+++ monitan/components/index.tsx
@@ -1,4 +1,4 @@
-import { Main } from "./components/Main.tsx";
+import { Main } from "./Main.tsx";
 
 function Index() {
   return (
